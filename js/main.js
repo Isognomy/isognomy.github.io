2d65c52b58cc59c8f4bc531227c63dd7055b8862
isognomy <isognomy@proton.me>
2025-03-08 21:17:48 -0800

    Testing some major changes

---
+++ js/main.js
@@ -0,0 +1,34 @@
+$(document).ready( function() {
+
+	// Logo
+var $logo = $('#logo');
+$logo.show(); // Always show the logo
+
+// Show logo 
+$('#tab-container .tab a').click(function() {
+    $logo.slideDown('slow');
+});
+function animMeter(){
+    $(".meter > span").each(function() {
+                $(this)
+                    .data("origWidth", $(this).width())
+                    .width(0)
+                    .animate({
+                        width: $(this).data("origWidth")
+                    }, 1200);
+            });
+}
+animMeter();
+
+      $('#tab-container').easytabs({
+        animate			: true,
+        updateHash		: true,
+        transitionIn	: 'slideDown',
+        transitionOut	: 'slideUp',
+        animationSpeed	: 800,
+        tabActiveClass	: 'active'}).bind('easytabs:midTransition', function(event, $clicked, $targetPanel){
+            if($targetPanel.selector=='#resume'){
+                    animMeter();
+            }
+        });
+    });
